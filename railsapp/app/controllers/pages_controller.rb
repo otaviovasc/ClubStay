095f5fe67f2bc6d6a489edf73b46b4e9c32208f2
otaviovasc-inteli <otavio.vasconcelos@sou.inteli.edu.br>
2024-02-17 15:06:09 -0300

    roi

--- railsapp/app/controllers/pages_controller.rb
+++ railsapp/app/controllers/pages_controller.rb
@@ -5,6 +5,13 @@ class PagesController < ApplicationController
     # Assuming you have scope or methods to filter customers and cards based on the parameters
     @customers = Customer.all
     @cards = Card.filter_by_params(params)
+
+    total_roi = @customers.sum(&:roi)
+    @average_roi = total_roi / @customers.count.to_f
+
+    total_ltv = @customers.sum(&:ltv)
+    total_ = @customers.sum(&:exti)
+
     respond_to do |format|
       format.html # For initial page load
       format.text { render partial: 'card_list', locals: { cards: @cards }, layout: false, formats: [:html] } # Assuming you have a _card_list.html.erb partial
