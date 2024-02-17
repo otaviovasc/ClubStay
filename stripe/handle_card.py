ecb3c097fe5a08487f3bd2379839e210e51562b6
Vitor Zucher <vzucher@gmail.com>
2024-02-17 14:04:20 -0300

    now yes

--- stripe/handle_card.py
+++ stripe/handle_card.py
@@ -1,2 +1,9 @@
-# gets cc information, read object
+import json
+
+ # get json object from rails app backend
+ 
+def get_json():
+    with open('card.json') as f:
+        data = json.load(f)
+    return data
 
