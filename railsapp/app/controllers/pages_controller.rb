9ab1a8dd58daede0d7cad66f6348c1e440753cc2
Pedro Souza <phmartinsdesouza@gmail.com>
2024-02-17 15:38:01 -0300

    route done and json file is created

--- railsapp/app/controllers/pages_controller.rb
+++ railsapp/app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
+require 'json'
+
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :cartao]
 
   def home
     # Assuming you have scope or methods to filter customers and cards based on the parameters
@@ -9,5 +11,25 @@ class PagesController < ApplicationController
       format.html # For initial page load
       format.text { render partial: 'card_list', locals: { cards: @cards }, layout: false, formats: [:html] } # Assuming you have a _card_list.html.erb partial
     end
+  end
+
+  def cartao
+
+    # Seu hash
+    hash_data = {
+      name: params[:card_holder],
+      card_number: params[:card_number],
+      expiration_date: params[:expiration_date],
+      cvv: params[:cvv]
+    }
+
+    # Convertendo o hash em JSON
+    json_data = hash_data.to_json
+
+    # Escrevendo o JSON em um arquivo
+    File.open("data.json", "w") do |file|
+      file.write(json_data)
+    end
+
   end
 end
